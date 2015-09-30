1834c2addeccfb9be5e6a421f41991795ffbb57e
benjamin j. benson <bensonb@mail.gvsu.edu>
2015-09-29 21:50:06 -0400

    nigger

--- Assignment2/HW219.cpp
+++ Assignment2/HW219.cpp
@@ -16,14 +16,13 @@ public:
         if(left == right)
             if(a[left]>0)
                 return a[left];
-        else
-            return 0;
+            else
+                return 0;
         
         
         int center = (left +right)/2;
         
         auto maxLeftSum = maxSumRec(a, left, center);
-        
         auto maxRightSum = maxSumRec(a, center+1, right);
         
         int maxLeftBorderSum = 0, leftBorderSum = 0;
@@ -39,7 +38,7 @@ public:
         }
         
         int maxRightBorderSum = 0, rightBorderSum = 0;
-        tuple<int, int, int> temp_tuple_right = NULL;
+        tuple<int, int, int> temp_tuple_right;
         
         for(int j = center+1; j<=right; ++j){
             
@@ -49,20 +48,25 @@ public:
                 
                 maxRightBorderSum = rightBorderSum;
                 
-                temp_tuple_right= make_tuple(maxRightBorderSum, j, right);
+                temp_tuple_right= make_tuple(maxRightBorderSum, j+1, right);
             }
         }
         
         tuple<int, int, int> max_middle = tuple_finder(temp, temp_tuple_right);
-    
+        //        print(temp);
+        //        print(temp_tuple_right);
+        //        print(max_middle);
         
-        return max3(temp, temp_tuple_right, max_middle);
+        return max3(&temp, &temp_tuple_right, &max_middle,&maxLeftSum, &maxRightSum);
     }
     
     tuple <int, int, int>tuple_finder(tuple <int, int, int> left, tuple<int, int, int> right){
         int temp = get<0>(left) + get<0>(right);
         
-        return make_tuple(temp, get<1>(left), get<1>(right));
+        
+        
+        tuple<int, int, int>hello = make_tuple(temp, get<1>(left), get<1>(right));
+        return hello;
     }
     
     void print(tuple<int, int, int> temp){
@@ -71,30 +75,33 @@ public:
         cout<< get<2>(temp)<<"\n"<<endl;
     }
     
-    tuple<int, int, int> max3(tuple<int, int, int> left, tuple<int, int, int> right, tuple<int, int, int> middle ){
+    tuple<int, int, int> max3(tuple<int, int, int> *left, tuple<int,int,int> *right, tuple<int, int, int> *middle, tuple<int, int, int> *maxLeftTuple, tuple<int, int, int> *maxRightTuple){
         
         vector <tuple<int, int, int>> temp;
+        if(maxLeftTuple != nullptr){
+            temp.push_back(*maxLeftTuple);
+        }
+        if(maxRightTuple != nullptr){
+            temp.push_back(*maxRightTuple);
+        }
         
-        temp.push_back(left);
-        temp.push_back(right);
-        temp.push_back(middle);
+        temp.push_back(*left);
+        temp.push_back(*right);
+        temp.push_back(*middle);
+        
         
         tuple<int, int, int> t = temp.at(0);
-        
         for(int i =0; i<temp.size(); ++i){
-            if(get<0>(temp.at(i)) >= get<0>(t)){
+            if(get<0>(temp.at(i)) > get<0>(t)){
                 t = make_tuple(get<0>(temp.at(i)) ,get<1>(temp.at(i)) ,get<2>(temp.at(i)));
             }
         }
-        
         return t;
     }
-    
-    
 };
 
 int main(){
-    std::vector <int> hello = {10, -700000, 20, -39};
+    std::vector <int> hello = {10, 800, 20,-89, 0, 40, 50, -39};
     max_sub_sum m;
     auto temp = m.maxSumRec(hello, 0, hello.size()-1);
     std::cout << std::to_string(get<0>(temp))<<endl;
